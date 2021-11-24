d82131b77d93d905cfbaadd05e79c3d1e7077026
austin sanchez <austinsanchez45@yahoo.com>
2021-11-24 13:06:52 -0600

    Added models

--- youtube_clone_project/youtube_app/models.py
+++ youtube_clone_project/youtube_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    videoId = models.CharField(max_length=25)
+    userComment = models.CharField(max_length=1000)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
