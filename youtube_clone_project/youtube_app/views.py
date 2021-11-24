8c207fc44e56bb4c60c103a4c9c5663d7dc1ec26
austin sanchez <austinsanchez45@yahoo.com>
2021-11-24 14:37:06 -0600

    Added path for comments and created class for comments

--- youtube_clone_project/youtube_app/views.py
+++ youtube_clone_project/youtube_app/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from rest_framework.serializers import Serializer
 from .models import Comment
@@ -14,3 +15,37 @@ class YoutubeList(APIView):
         youtube = Comment.objects.all()
         serializer = YoutubeSerializer(youtube, many = True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = YoutubeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Comments(APIView):
+    
+    def get(self, request, pk):
+        comment = self.get_Comment(pk)
+        serializer = YoutubeSerializer(comment)
+        return Response(serializer.data)
+    
+    def get_Comment(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except:
+            raise Http404
+        
+    def put(self, request, pk):
+        comment = self.get_Comment(pk)
+        serializer = YoutubeSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        comment = self.get_Comment(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
