b4a459fd5906b845bf5df6c5d50832c59c147f8b
austin sanchez <austinsanchez45@yahoo.com>
2021-11-30 12:51:49 -0600

    Created views for replying

--- youtube_clone_project/youtube_app/views.py
+++ youtube_clone_project/youtube_app/views.py
@@ -1,6 +1,8 @@
 from django.http.response import Http404
 from .models import BackendData
+from .models import Comments
 from .serializers import YoutubeSerializer
+from .serializers import ReplySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -46,3 +48,44 @@ class VideoDetails(APIView):
         video_details = self.get_video_details(pk)
         video_details.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+class ReplyList(APIView):
+
+    def get(self, request):
+        reply = Comments.objects.all()
+        serializer = ReplySerializer(reply, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ReplyDetail(APIView):
+
+    def get_object(self, commentid):
+        try:
+            return Comments.objects.get(commentid=commentid)
+        except Comments.DoesNotExist:
+            raise Http404
+
+    def get(self, request, commentid):
+        reply = self.get_object(commentid)
+        serializer = ReplySerializer(reply)
+        return Response(serializer.data)
+
+    def put(self, request, commentid):
+        reply = self.get_object(commentid)
+        serializer = ReplySerializer(reply, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, commentid):
+        reply = self.get_object(commentid)
+        reply.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
